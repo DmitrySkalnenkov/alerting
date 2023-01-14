9d1641e8ec57f812d7df156b67b6ac01ff19fd3e
DmitrySkalnenkov <dmitry.skalnenkov@gs-labs.ru>
2023-01-14 03:41:29 +0300

    Added metric stucture

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,56 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"runtime"
+)
+
+type Metrics struct {
+	Alloc,
+	BuckHashSys,
+	Frees,
+	GCCPUFraction,
+	GCSys,
+	HeapAlloc,
+	HeapIdle,
+	HeapInuse,
+	HeapObjects,
+	HeapReleased,
+	HeapSys,
+	LastGC,
+	Lookups,
+	MCacheInuse,
+	MCacheSys,
+	MSpanInuse,
+	MSpanSys,
+	Mallocs,
+	NextGC,
+	NumForcedGC,
+	NumGC,
+	OtherSys,
+	PauseTotalNs,
+	StackInuse,
+	StackSys,
+	Sys,
+	TotalAlloc,
+	RandomValue float64
+	PollCount int
+}
+
+func main() {
+
+	var m Metrics
+	var rtm runtime.MemStats
+	runtime.ReadMemStats(&rtm)
+	m.Alloc = float64(rtm.Alloc)
+	fmt.Println(m.Alloc)
+
+	client := http.Client{}
+	response, err := client.Get("https://golang.org")
+	if err == nil {
+		fmt.Println(response)
+	} else {
+		fmt.Println("Error %s", err)
+	}
+}
