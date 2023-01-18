c8db7590b4dd19a7de7954685723b7316c5164aa
dmitryskalnenkov <dmitry.skalnenkov@gs-labs.ru>
2023-01-18 15:47:35 +0300

    + Server functionality

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -159,44 +159,9 @@ func main() {
 	serverTcpPort := 8080
 	var PollCount int64
 	var rtm runtime.MemStats
-<<<<<<< HEAD
-	runtime.ReadMemStats(&rtm)
-	m.Alloc = float64(rtm.Alloc)
-	m.BuckHashSys = float64(rtm.BuckHashSys)
-	m.Frees = float64(rtm.Frees)
-	m.GCCPUFraction = float64(rtm.GCCPUFraction)
-	m.GCSys = float64(rtm.GCSys)
-	m.HeapAlloc = float64(rtm.HeapAlloc)
-	m.HeapIdle = float64(rtm.HeapIdle)
-	m.HeapInuse = float64(rtm.HeapInuse)
-	m.HeapObjects = float64(rtm.HeapObjects)
-	m.HeapReleased = float64(rtm.HeapReleased)
-	m.HeapSys = float64(rtm.HeapSys)
-	m.LastGC = float64(rtm.LastGC)
-	m.Lookups = float64(rtm.Lookups)
-	m.MCacheInuse = float64(rtm.MCacheInuse)
-	m.MCacheSys = float64(rtm.MCacheSys)
-	m.MSpanInuse = float64(rtm.MSpanSys)
-	m.MSpanSys = float64(rtm.MSpanSys)
-	m.Mallocs = float64(rtm.Mallocs)
-	m.NextGC = float64(rtm.NextGC)
-	m.NumForcedGC = float64(rtm.NumForcedGC)
-	m.NumGC = float64(rtm.NumGC)
-	m.OtherSys = float64(rtm.OtherSys)
-	m.PauseTotalNs = float64(rtm.PauseTotalNs)
-	m.StackInuse = float64(rtm.StackInuse)
-	m.StackSys = float64(rtm.StackSys)
-	m.Sys = float64(rtm.Sys)
-	m.TotalAlloc = float64(rtm.TotalAlloc)
-	//	m.RandomValue = float64(rtm.RandomValue)
-	//	m.PollCount = int(rtm.PollCount)
-
-	fmt.Println(m)
-=======
 	var MetricArray [29][3]string
 	client := &http.Client{}
 	client.Timeout = 100 * time.Millisecond
->>>>>>> tmp
 
 	for {
 		time.Sleep(100 * time.Millisecond)
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+// HelloWorld — обработчик запроса.
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello, World</h1>"))
+}
+
+func main() {
+
+	server := &http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/", HelloWorld)
+	server.ListenAndServe()
+}
