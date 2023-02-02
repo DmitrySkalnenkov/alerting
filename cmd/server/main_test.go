5b4431202c84f29dbd88f69c304411723b030687
dmitryskalnenkov <dmitry.skalnenkov@gs-labs.ru>
2023-02-02 14:54:13 +0300

    Added unit test for func contains()

--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -121,3 +121,48 @@ func TestPushCounter(t *testing.T) {
 		})
 	}
 }
+
+func TestContains(t *testing.T) {
+	//func contains(s []string, str string) bool {}
+	type inputs struct {
+		stringArray []string
+		stringValue string
+	}
+
+	tests := []struct {
+		name  string
+		input inputs
+		want  bool
+	}{ //Test table
+		{
+			name: "Positve test",
+			input: inputs{
+				stringArray: []string{
+					"Alloc",
+					"BuckHashSys",
+					"Frees"},
+				stringValue: "BuckHashSys",
+			},
+			want: true,
+		},
+		{
+			name: "Negative test",
+			input: inputs{
+				stringArray: []string{
+					"Alloc",
+					"BuckHashSys",
+					"Frees"},
+				stringValue: "UnknownMetric",
+			},
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if contains(tt.input.stringArray, tt.input.stringValue) != tt.want {
+				t.Errorf("StringArray is %s , StringValue is %s, want is %t", tt.input.stringArray, tt.input.stringValue, tt.want)
+			}
+		})
+	}
+}
