6658838f1f84fd26f0e9f43a7f5081fde5618753
dmitryskalnenkov <dmitry.skalnenkov@gs-labs.ru>
2023-02-06 16:18:16 +0300

    Working GuageHandler and CounterHandler unit-tests.

--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -250,3 +250,92 @@ func TestGaugesHandler(t *testing.T) {
 		})
 	}
 }
+
+//func CountersHandler(w http.ResponseWriter, r *http.Request) {
+func TestCountersHandler(t *testing.T) {
+	type want struct {
+		contentType string
+		code        int
+		response    string
+	}
+	tests := []struct {
+		name    string
+		request string
+		want    want
+	}{ //Test table
+		{
+			name:    "positive test #1",
+			request: "http://127.0.0.1:8080/update/counter/TestMetric/12421234123",
+			want: want{
+				contentType: "text/plain",
+				code:        200,
+				response:    `{"status":"ok"}`,
+			},
+		},
+		{
+			name:    "positive test #2",
+			request: "http://127.0.0.1:8080/update/counter/TestMetric/-232131123",
+			want: want{
+				contentType: "text/plain",
+				code:        200,
+				response:    `{"status":"ok"}`,
+			},
+		},
+		{
+			name:    "null metric name ",
+			request: "http://127.0.0.1:8080/update/counter/",
+			want: want{
+				contentType: "text/plain",
+				code:        404,
+				response:    `{"status":"ok"}`,
+			},
+		},
+		{
+			name:    "bad request",
+			request: "http://127.0.0.1:8080/update/counter",
+			want: want{
+				contentType: "text/plain",
+				code:        400,
+				response:    `{"status":"ok"}`,
+			},
+		},
+		{
+			name:    "symbolic value",
+			request: "http://127.0.0.1:8080/update/counter/Metric/rqwer",
+			want: want{
+				contentType: "text/plain",
+				code:        400,
+				response:    `{"status":"ok"}`,
+			},
+		},
+		{
+			name:    "float value",
+			request: "http://127.0.0.1:8080/update/counter/Metric/666.3",
+			want: want{
+				contentType: "text/plain",
+				code:        400,
+				response:    `{"status":"ok"}`,
+			},
+		},
+	}
+	for _, tt := range tests {
+		// запускаем каждый тест
+		t.Run(tt.name, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodPost, tt.request, nil)
+			w := httptest.NewRecorder()
+			h := http.HandlerFunc(GaugesHandler)
+			h.ServeHTTP(w, req)
+			res := w.Result()
+			defer res.Body.Close()
+			GaugesHandler(w, req)
+			resBody, err := io.ReadAll(res.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+			fmt.Printf("Status is %s, status code is %d, body is %s. \n", w.Result().Status, w.Result().StatusCode, string(resBody))
+			if res.StatusCode != tt.want.code {
+				t.Errorf("Expected status code %d, got %d", tt.want.code, res.StatusCode)
+			}
+		})
+	}
+}
