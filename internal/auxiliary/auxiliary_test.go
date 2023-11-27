6dbe07fd347f4e394ba67650180fb6b4e2cec4f0
dmitryskalnenkov <dmitry.skalnenkov@gs-labs.ru>
2023-11-27 21:21:26 +0300

    + UnitTest for auxiliary (all unit test are working)

--- internal/auxiliary/auxiliary_test.go
+++ internal/auxiliary/auxiliary_test.go
@@ -6,19 +6,76 @@ import (
 	"testing"
 )
 
-// TODO Make tests for auxiliary funcs
-const SetValue = "set_val"
-
-func giveMeAFunc(isSet bool, k string) func(string) error {
-	return func(k string) error {
-		if isSet {
-			fmt.Printf("TESTDEBUG: Will be used Setenv(%s)", k)
-			return os.Setenv(k, SetValue)
-		} else {
-			fmt.Printf("TESTDEBUG: Will be used Unsetenv(%s)", k)
-			return os.Unsetenv(k)
-		}
+func TestGetParamValue(t *testing.T) {
+	type inputs struct {
+		envVarName         string
+		envVarDefaultValue string
+		paramFlagName      string
 	}
+
+	tests := []struct {
+		name            string
+		input           inputs
+		isSetEnvVarFunc bool
+		setEnvValue     string
+		setParamValue   string
+		want            string
+	}{ //Test table
+		{
+			name: "Positive test. The parameter value is , the environment variable isn't set",
+			input: inputs{
+				envVarName:         "TEST_ENV_VAR",
+				envVarDefaultValue: "test_default_value1",
+				paramFlagName:      "t1",
+			},
+			isSetEnvVarFunc: false,
+			setEnvValue:     "set_env_val",
+			setParamValue:   "set_param_val",
+			want:            "set_param_val",
+		},
+		{
+			name: "Positive test. The parameter value is set, the environment variable is set too",
+			input: inputs{
+				envVarName:         "TEST_ENV_VAR",
+				envVarDefaultValue: "test_default_value2",
+				paramFlagName:      "t1",
+			},
+			isSetEnvVarFunc: true,
+			setEnvValue:     "set_env_val",
+			setParamValue:   "set_param_val",
+			want:            "set_env_val",
+		},
+		{
+			name: "Positive test. The parameter value is set, the environment variable is empty",
+			input: inputs{
+				envVarName:         "TEST_ENV_VAR",
+				envVarDefaultValue: "test_default_value3",
+				paramFlagName:      "t1",
+			},
+			isSetEnvVarFunc: true,
+			setEnvValue:     "",
+			setParamValue:   "set_param_val",
+			want:            "test_default_value3",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.isSetEnvVarFunc {
+				fmt.Printf("TESTDEBUG: Will be used Setenv(%s)\n", tt.input.envVarName)
+				os.Setenv(tt.input.envVarName, tt.setEnvValue)
+			} else {
+				fmt.Printf("TESTDEBUG: Will be used Unsetenv(%s)\n", tt.input.envVarName)
+				os.Unsetenv(tt.input.envVarName)
+			}
+			argStr := "-" + tt.input.paramFlagName + "=" + tt.setParamValue
+			os.Args = append(os.Args, argStr)
+			if GetParamValue(tt.input.envVarName, tt.input.paramFlagName, tt.input.envVarDefaultValue, "") != tt.want {
+				t.Errorf("TEST_ERROR: input value is %v,  want is %s ", tt.input, tt.want)
+			}
+		})
+	}
+
 }
 
 func TestGetEnvVariable(t *testing.T) {
@@ -31,6 +88,7 @@ func TestGetEnvVariable(t *testing.T) {
 		name            string
 		input           inputs
 		isSetEnvVarFunc bool
+		setValue        string
 		want            string
 	}{ //Test table
 		{
@@ -40,8 +98,9 @@ func TestGetEnvVariable(t *testing.T) {
 				envVarDefaultValue: "test_default_value1",
 			},
 			isSetEnvVarFunc: true,
+			setValue:        "set_val",
 			//f:    giveMeAFunc(true, "TEST_ENV_VAR"),
-			want: SetValue,
+			want: "set_val",
 		},
 		{
 			name: "Default value test when var value is empty",
@@ -50,6 +109,7 @@ func TestGetEnvVariable(t *testing.T) {
 				envVarDefaultValue: "test_default_value2",
 			},
 			isSetEnvVarFunc: true,
+			setValue:        "",
 			want:            "test_default_value2",
 		},
 		{
@@ -59,19 +119,26 @@ func TestGetEnvVariable(t *testing.T) {
 				envVarDefaultValue: "test_default_value2",
 			},
 			isSetEnvVarFunc: false,
+			setValue:        "set_val",
 			want:            "test_default_value2",
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			if tt.isSetEnvVarFunc {
+				fmt.Printf("TESTDEBUG: Will be used Setenv(%s)\n", tt.input.envVarName)
+				os.Setenv(tt.input.envVarName, tt.setValue)
+			} else {
+				fmt.Printf("TESTDEBUG: Will be used Unsetenv(%s)\n", tt.input.envVarName)
+				os.Unsetenv(tt.input.envVarName)
+			}
 			if GetEnvVariable(tt.input.envVarName, tt.input.envVarDefaultValue) != tt.want {
-				giveMeAFunc(tt.isSetEnvVarFunc, tt.input.envVarName)
 				t.Errorf("TEST_ERROR: input value is %v,  want is %s ", tt.input, tt.want)
 			}
 		})
 	}
 
-	t.Setenv("TEST_ENV_VAR", "test_val")
 }
 
 func TestTrimQuotes(t *testing.T) {
